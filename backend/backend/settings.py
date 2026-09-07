8a567acf8f8766f6672e0a2c4daf17da1f8052b8
Amusa Yusuf Olanrewaju <crystalclearitms@gmail.com>
2026-09-07 20:37:21 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -270,19 +270,19 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "https://sensational-interior-07.onrender.com",
-# ]
-
 CORS_ALLOWED_ORIGINS = [
-    origin.strip()
-    for origin in os.getenv(
     "http://localhost:5173",
     "https://sensational-interior-07.onrender.com",
-    ).split(",")
-    if origin.strip()
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     origin.strip()
+#     for origin in os.getenv(
+#     "http://localhost:5173",
+#     "https://sensational-interior-07.onrender.com",
+#     ).split(",")
+#     if origin.strip()
+# ]
 
 
 # CORS_ALLOWED_ORIGINS = [
