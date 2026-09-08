a34bee8ffdf2bf38ba255fe82f83716afe997154
Amusa Yusuf Olanrewaju <crystalclearitms@gmail.com>
2026-09-08 19:55:31 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -275,9 +275,23 @@ STORAGES = {
 # MEDIA FILES
 # ============================================================
 
+# MEDIA_URL = "/media/"
+
+# MEDIA_ROOT = BASE_DIR / "media"
+
+# ============================================================
+# MEDIA FILES (CLOUDINARY)
+# ============================================================
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
 # ============================================================
