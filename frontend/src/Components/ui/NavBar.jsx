bb47d2434ed05de1b016772a1e3e9e9dcc5c66c6
Amusa Yusuf Olanrewaju <crystalclearitms@gmail.com>
2026-09-09 16:30:15 +0100

    Update NavBar.jsx

--- frontend/src/Components/ui/NavBar.jsx
+++ frontend/src/Components/ui/NavBar.jsx
@@ -1,4 +1,3 @@
-```jsx
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import {
@@ -178,4 +177,4 @@ export default function NavBar() {
     </>
   );
 }
-```
+
