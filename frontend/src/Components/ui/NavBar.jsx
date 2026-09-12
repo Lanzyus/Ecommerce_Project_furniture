14a48530c5402b3b138382c6086d70a697cb85ad
Amusa Yusuf Olanrewaju <crystalclearitms@gmail.com>
2026-09-12 22:16:55 +0100

    Update NavBar.jsx

--- frontend/src/Components/ui/NavBar.jsx
+++ frontend/src/Components/ui/NavBar.jsx
@@ -131,7 +131,7 @@ export default function NavBar() {
                   href="/#projects"
                   onClick={close}
                 >
-                  Projects
+                  Buy Products
                 </a>
 
                 <a
